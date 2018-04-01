3955a19645f7a477eb753f1113fc92b4e087bccb
nfazleen <nurul.fazleen77@gmail.com>
2018-04-01 03:40:28 -0400

    client java code

---
+++ Client.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.net.*;
+public class Client
+{
+public static void main(String[]args)throws Exception
+{
+Socket sock = new Socket("192.168.189.129",36744);
+BufferedReader keyRead = new BufferedReader(new 
+InputStreamReader(System.in));
+
+OutputStream ostream = sock.getOutputStream();
+PrintWriter pwrite = new PrintWriter(ostream,true);
+
+InputStream istream=sock.getInputStream();
+BufferedReader receiveRead=new BufferedReader(new 
+InputStreamReader(istream));
+
+System.out.println("Enter String:");
+
+String receiveMessage,sendMessage;
+while(true)
+{
+sendMessage=keyRead.readLine();
+pwrite.println(sendMessage);
+pwrite.flush();
+
+if((receiveMessage=receiveRead.readLine())!=null)
+{
+System.out.println(receiveMessage);
+}
+}
+}
+}
+#assignment1client
